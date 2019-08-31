e50a4aa097c4a1738a98c6f819d335cebf6194bd
Marcos Á. Medina <marcmedi1@hotmail.com>
2019-08-31 15:20:31 -0300

    Images displaying

--- ReactApp/confusion/src/App.js
+++ ReactApp/confusion/src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 // import logo from './logo.svg';
 import { Navbar, NavbarBrand } from "reactstrap";
 import './App.css';
+import Menu from "./components/menuComponent";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Con Fusion Restorant Banner</NavbarBrand>
         </div>
       </Navbar>
+      <Menu></Menu>
     </div>
   );
 }
