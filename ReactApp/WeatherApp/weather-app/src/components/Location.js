474d524f959ccf226b68be1fc7c1663d06ca6381
Marcos Á. Medina <marcmedi1@hotmail.com>
2019-09-11 18:57:03 -0300

    Destructuring 1

--- ReactApp/WeatherApp/weather-app/src/components/Location.js
+++ ReactApp/WeatherApp/weather-app/src/components/Location.js
@@ -1,9 +1,11 @@
 import React from "react"
 
-const Location = (props) => (
-    <div>
-        <h1>{props.city}</h1>
-    </div>
-);
-
+const Location = (props) => {    
+    const {city} = props;
+    return (
+        <div>
+            <h1>{city}</h1>
+        </div>
+    );
+}
 export default Location;
