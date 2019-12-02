98c37b31ed565f7af577e698829a829132fa3bc9
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-12-02 15:27:48 -0300

    PLaying with the state in React

--- ReactApp/WeatherApp/weather-app/src/components/WeatherLocation/index.js
+++ ReactApp/WeatherApp/weather-app/src/components/WeatherLocation/index.js
@@ -16,18 +16,42 @@ const data = {
     weatherState: FOG,
     wind: 10
 }
+const data2 = {
+    humidty: 100,
+    temperature: 22,
+    weatherState: RAIN,
+    wind: 20
+}
 
 class WeatherLocation extends Component {
 
-    updateClickFunction(){
+    constructor(){
+        super();
+        this.state = {
+            location: {
+                city: "Miami",
+                neighbor: "Wynwood"
+            },
+            data: data
+        }
+    }
+
+    updateClickFunction = () => {
         console.log("updated!")
+        this.setState({
+            location: {
+                city: "Miami",
+                neighbor: "Wynwood"
+            },
+            data: data2
+        });
     }
 
     render(){
         return(
             <div className="weatherLocationContainer">
-                <Location city={"Miami"} neighbor={"Wynwood"}></Location>
-                <WeatherData data={data}></WeatherData>
+                <Location city={this.state.location.city} neighbor={this.state.location.neighbor}></Location>
+                <WeatherData data={this.state.data}></WeatherData>
                 <button onClick={this.updateClickFunction}>Update Info</button>
             </div>
         );
